3074a8a4b6a58767536bc4b8508d80d3730bb218
Darryl Roger Carvalho <rbootstrap@gmail.com>
2017-07-14 12:11:00 +0530

    Add files via upload

---
+++ arctic/js/custom.js
@@ -0,0 +1,115 @@
+jQuery(document).ready(function($){
+	
+	"use strict";
+	
+	
+	/*==========================*/	
+	/********* Waypoints ********/	
+	/*==========================*/
+	var scrolling = false;
+	function onScrollInit( items, trigger ) {
+		items.each( function() {
+		var osElement = $(this),
+		osAnimationClass = osElement.attr('data-os-animation'),
+		osAnimationDuration = osElement.attr('data-os-animation-duration');
+	
+		osElement.css({
+			'-webkit-animation-duration':  osAnimationDuration,
+			'-moz-animation-duration':     osAnimationDuration,
+			'animation-duration':          osAnimationDuration
+		});
+	
+		var osTrigger = ( trigger ) ? trigger : osElement;
+			
+		osTrigger.waypoint(function() {
+			osElement.addClass('animated').addClass(osAnimationClass);
+		},{
+			triggerOnce: true,
+			offset: '95%'
+			});
+		});
+	}
+	
+	$(window).on('scroll', function(){
+		if(!scrolling){
+			onScrollInit( $('.os-animation') );
+			scrolling = true;
+		}
+	});
+	
+	onScrollInit( $('.os-animation') );
+	onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
+	
+	/*==========================*/	
+	/***** Smooth Scrolling *****/	
+	/*==========================*/
+	// Add smooth scrolling to all links in navbar + Section Links and the Slider
+	$(".navbar a, section a").on('click', function(event) {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+	
+		// Store hash
+		var hash = this.hash;
+	
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (1500) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 1500, function(){
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+		});
+	});
+	
+	/*=================================================================*/	
+	/******** Fix Placeholder Issue in IE9 and other browsers *******/	
+	/*=================================================================*/
+	$('input, textarea').placeholder();
+	
+	
+	/*==========================*/	
+	/******** Back To Top *******/	
+	/*==========================*/
+	var offset = 220;
+	var duration = 1500;
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery('.back-to-top').fadeIn(duration);
+		} else {
+			jQuery('.back-to-top').fadeOut(duration);
+		}
+	});
+					
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
+	
+	
+});	
+	
+	/*==========================*/	
+	/******** Google Map *******/	
+	/*==========================*/
+	if($('#map-location').length){
+		var map;
+		map = new GMaps({
+			el: '#map-location',
+			zoom: 15,
+			scrollwheel:false,
+			//Set Latitude and Longitude Here
+			lat: 34.4143704,
+			lng: -119.6967133
+		});
+		  
+		//Add map Marker
+		map.addMarker({
+			icon: 'images/icon/location_icon.png',
+			lat: 34.4143704,
+			lng: -119.6967133,
+			infoWindow: {
+				content: '<p class="map-info text-center"><strong>Artic Agency</strong> <br/>310 Chapala St, Santa Barbara, CA 93101<br/> +1 805-966-9180 </p>'
+			}
+		});
+	}
---
+++ arctic/js/form-scripts.js
@@ -0,0 +1,55 @@
+$("#contactForm").validator().on("submit", function (event) {
+	if (event.isDefaultPrevented()) {
+		// handle the invalid form...
+		formError();
+		submitMSG(false, "Please Fill in all the details. Could not send mail!");
+	} else {
+		// everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+	// Initiate Variables With Form Content
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var subject = $("#subject").val();
+	var message = $("#message").val();
+
+	$.ajax({
+		type: "POST",
+		//Set the Path to Your PHP File here
+		url: "/arctic/form-process.php",
+		data: "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message,
+        success : function(text){
+			if (text == "success"){
+				formSuccess();
+				$("#contact_form_fields").slideUp(); //hide form after success
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+		}
+	});
+}
+
+
+function formSuccess(){
+	$("#contactForm")[0].reset();
+	submitMSG(true, "Thank you for your message! We will get back to you shortly!")
+}
+
+function formError(){
+	$(this).removeClass();
+}
+
+function submitMSG(valid, msg){
+	if(valid){
+		var msgClasses = "h3 text-center text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+	$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
